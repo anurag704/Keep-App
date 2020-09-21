a1590276608b93fed0c1cce62e08ee47b1163083
anurag704 <anuragjha1093@gmail.com>
2020-09-21 18:05:11 +0530

    Updates

---
+++ precache-manifest.8068a891930d834453c39c308473d878.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54062adb5ca82986212ee1f6b9f73c88",
+    "url": "/Keep-App/index.html"
+  },
+  {
+    "revision": "9e9622f0ed4a99d71405",
+    "url": "/Keep-App/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "b96e74e8a58a4327ba3e",
+    "url": "/Keep-App/static/css/main.e7f45c5d.chunk.css"
+  },
+  {
+    "revision": "9e9622f0ed4a99d71405",
+    "url": "/Keep-App/static/js/2.16a07508.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Keep-App/static/js/2.16a07508.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b96e74e8a58a4327ba3e",
+    "url": "/Keep-App/static/js/main.e9854011.chunk.js"
+  },
+  {
+    "revision": "83fd63ffd651ecd98aa6",
+    "url": "/Keep-App/static/js/runtime-main.c58462d7.js"
+  },
+  {
+    "revision": "8691bd8a5aacc3939da4994bc457f381",
+    "url": "/Keep-App/static/media/logo.8691bd8a.png"
+  }
+]);
